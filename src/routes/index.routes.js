337bbcdfd2e8a8f4d7baa47cc774efe0a20e47f9
Vinicius Valverde <vinicius.valverde@aluno.senai.br>
2025-05-13 08:22:39 -0300

    refactor: adjust project

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -2,10 +2,6 @@ import express from "express";
 
 // Importar todas as rotas
 import authRouter from "./auth.routes.js";
-import animesRouter from "./animeRoutes.js";
-import personagensRouter from "./personagemRoutes.js";
-import collectionRouter from "./collectionRoutes.js";
-import cardRouter from "./cardRoutes.js";
 
 import authMiddleware from "../middleware/authMiddleware.js";
 
@@ -13,13 +9,8 @@ const router = express.Router();
 
 // Rotas públicas
 router.use("/auth", authRouter);
-router.use("/collections", collectionRouter);
-router.use("/cards", cardRouter);
 
 // Rotas protegidas
 router.use(authMiddleware);
-
-router.use("/animes", animesRouter);
-router.use("/personagens", personagensRouter);
 
 export default router;
