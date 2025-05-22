c68b95afa6dc41fa8055eda129bf77808fd4aef3
vinicius valverde <vinicius.valverde@aluno.senai.br>
2025-05-22 09:18:50 -0300

    feat: add getALl users method in route

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,6 +3,7 @@ import AuthController from "../controllers/authController.js";
 
 const authRouter = express.Router();
 
+authRouter.get("/getUsers", AuthController.getAllUsers)
 authRouter.post("/register", AuthController.register);
 authRouter.post("/login", AuthController.login);
 
